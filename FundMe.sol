5500ba05f23b28534bb6a8222c90500e45bbea3b
betoayza <aaayza@gmail.com>
2023-04-11 21:19:46 -0300

    FoundMe updated

--- FundMe.sol
+++ FundMe.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >= 0.8.0
+pragma solidity >= 0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-    mapping(address => unint256) public addressToMountFunded;
+    mapping(address => uint256) public addressToMountFunded;
+
+    function fund() public payable {
+        addressToMountFunded[msg.sender] += msg.value;
+    }
+
+    function getVersion() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43); // BTC-USD
+        return priceFeed.version();
+    }
 }
