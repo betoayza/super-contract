b83328dc6e075b799d673f92938891080d3c5877
betoayza <aaayza@gmail.com>
2023-04-20 21:24:17 -0300

    Modifiers added

--- FundMe.sol
+++ FundMe.sol
@@ -6,27 +6,28 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract FundMe {
     mapping(address => uint256) public addressToMountFunded; // muestra el monto envíado por la address actual    
     address[] funders;
+    address payable owner;
 
-    // constructor() public {
-    //     owner = msg.sender;
-    // }
+    constructor() payable {
+        owner = payable(msg.sender);
+    }
 
     // modificadores
-    //modifier onlyOwner { // se asegura que la funcion que lo utiliza cumpla ciertos comportamientos o condiciones
-        // require(msg.sender == owner); // solo el propietario del contrato realizar tal función
-        //_; // ejecuta el resto del código de la función que lo utiliza
-    //}
-
-    /*
-    modifier checkBalance {
-        require(address(this).balance >= _amount, "No hay saldo suficiente...");
+    modifier onlyOwner { // se asegura que la funcion que lo utiliza cumpla ciertos comportamientos o condiciones
+        require(payable(msg.sender) == owner, "You are not the contract owner..."); // solo el propietario del contrato realizar tal función
+        _; // ejecuta el resto del código de la función que lo utiliza
     }
-    */
+    
+    modifier checkBalance {
+        require(address(this).balance > 0, "Not enough balance...");
+        _;
+    }
+    
 
     // ENVIAR ACTIVOS AL CONTRATO
     function fund() public payable {        
         uint256 minWEIs = 20 * 10 ** 18; // minimo fondeo: 20 dolares en WEIs
-        require(getConversionRate(msg.value) >= minWEIs, "Tiene que fondear mas dolares: minimo 40");
+        require(getConversionRate(msg.value) >= minWEIs, "Tiene que fondear mas dolares: minimo 20");
         addressToMountFunded[msg.sender] += msg.value; // agregar al mapping 'address->monto'
         funders.push(msg.sender); // agregar address al array de fondeadores
     }
@@ -43,15 +44,14 @@ contract FundMe {
     }
 
     function getConversionRate(uint256 _ethAmount) public view returns(uint256){ // obtener el precio en USD
-        uint256 ethPrice = getPrice(); 
-        uint256 ethAmountInUSD = (ethPrice * _ethAmount) / 1000000000000000000;
+        uint256 ethPriceInWEIs = getPrice(); 
+        uint256 ethAmountInUSD = (ethPriceInWEIs * _ethAmount) / 1000000000000000000;
         return ethAmountInUSD;
     } 
 
     // RETIRAR ACTIVOS DEL CONTRATO
-    function withdraw() payable public { 
-        address payable sender = payable(msg.sender); // castear address a address payable       
-        sender.transfer(address(this).balance); // address(this) es la direccion misma del contrato de la cual se extrerá los fondos
+    function withdraw() public payable onlyOwner checkBalance {         
+        owner.transfer(address(this).balance); // transferir al propietario del contrato el balance del contrato 
     } 
 }
 
